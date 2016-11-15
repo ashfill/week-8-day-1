5690fb913a0f8223b4df2fbedfd82d545f7ce46f
Ashton Fillery <ashtonfillery@gmail.com>
2016-11-14 17:07:07 -0800

    Intial commit

---
+++ Homework/Homework/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Homework.Models;
+
+namespace Homework.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] cars = new Product[]
+        {
+           new Product {ID = 1, Make = "Suburu", Model = "WRX", Price = 35.000m },
+           new Product {ID = 2, Make = "Audi", Model = "S7", Price = 40.000m },
+           new Product {ID = 3, Make = "Aston Martin", Model = "Vanquish", Price = 50.000m },
+           new Product {ID = 4, Make = "Honda", Model = "Type R", Price = 35.000m }
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return cars;
+        }
+        public IHttpActionResult GetProducts(int id)
+        {
+            var car = cars.SingleOrDefault(p => p.ID == id);
+
+            if (car == null)
+                return NotFound();
+
+            return Ok(car);
+        }
+
+        public IHttpActionResult GetProductByModel(string cay)
+        {
+            var car = cars.SingleOrDefault(p => p.Model.ToLower() == cay.ToLower());
+
+            if (car == null)
+                return NotFound();
+
+            return Ok(car);
+        }
+    }
+}
---
+++ Homework/Homework/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+    }
+}
